b9555ba733153e0b7fce4c61cd0c7fa95de1d661
xie <xieshuhua@163.com>
2019-11-04 22:25:46 +0800

    init

---
+++ test_realm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test_realm
+//
+//  Created by xie on 2019/11/4.
+//  Copyright © 2019 xie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, guoguo!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
